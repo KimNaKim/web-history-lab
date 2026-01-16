8de363850f535d456cf16d953bc6316aeb999fb0
KimNaKim <shila66@naver.com>
2026-01-16 17:32:22 +0900

    v4

---
+++ v4/src/main/java/com/example/v4/UserController.java
@@ -0,0 +1,24 @@
+package com.example.v4;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+public class UserController {
+
+    @GetMapping("/join")
+    public String join() {
+        return "join 완료";
+    }
+    
+
+    @GetMapping("/login")    
+    public String login(){
+        System.out.println("login 호출됨");
+        return "Login 호출";
+    }
+    
+}
